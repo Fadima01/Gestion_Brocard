fd7c2e83ba7b77c2aef04d82990ba80ee98f645b
Fadima Ouane <fadimaouane@gmail.com>
2026-06-21 19:27:39 +0000

    Create admin user

--- build.sh
+++ build.sh
@@ -7,3 +7,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate --noinput
+
+python manage.py shell -c "from django.contrib.auth import get_user_model; User=get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin','fadimaouane@gmail.com','Brocard2026@')"
