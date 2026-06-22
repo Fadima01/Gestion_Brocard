113236ee99178bd9ab426b67e774fa9bf5e81112
Fadima Ouane <fadimaouane@gmail.com>
2026-06-22 10:29:00 +0000

    Reset admin user

--- build.sh
+++ build.sh
@@ -8,3 +8,29 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate --noinput
 
+python manage.py shell << EOF
+from apps.users.models import User
+
+username = "fadima"
+email = "fadimaouane@gmail.com"
+password = "Brocard2026@"
+
+user, created = User.objects.get_or_create(
+    username=username,
+    defaults={
+        "email": email,
+        "is_staff": True,
+        "is_superuser": True,
+        "role": "ADMIN"
+    }
+)
+
+user.email = email
+user.is_staff = True
+user.is_superuser = True
+user.role = "ADMIN"
+user.set_password(password)
+user.save()
+
+print("Superuser créé ou mis à jour avec succès")
+EOF
